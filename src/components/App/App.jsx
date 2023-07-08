e71339ecf5b0f0c31c0e445c81c563793c8090dd
Serhii Voitsekhovskyi <95.ssh.ssh@gmail.com>
2023-07-08 12:22:36 +0200

    add redirect

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import { lazy } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import SharedLayout from 'components/SharedLayout/SharedLayout';
 
 const Home = lazy(() => import('pages/Home/Home'));
@@ -7,7 +7,7 @@ const Movies = lazy(() => import('pages/Movies/Movies'));
 const MovieDetails = lazy(() => import('pages/MovieDetails/MovieDetails'));
 const Cast = lazy(() => import('components/Cast/Cast'));
 const Reviews = lazy(() => import('components/Reviews/Reviews'));
-const NotFound = lazy(() => import('pages/NotFound/NotFound'));
+// const NotFound = lazy(() => import('pages/NotFound/NotFound'));
 
 const App = () => {
   return (
@@ -20,7 +20,8 @@ const App = () => {
           <Route path="reviews" element={<Reviews />} />
         </Route>
       </Route>
-      <Route path="*" element={<NotFound />} />
+      {/* <Route path="*" element={<NotFound />} /> */}
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
 };
